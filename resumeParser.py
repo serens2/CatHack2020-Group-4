2d1b1ecd1e4b3bdee82f286a342e8cb4995d6cfe
boyanc <boyan_claire@cat.com>
2020-07-16 12:52:55 -0500

    text extractor for PDF

---
+++ resumeParser.py
@@ -0,0 +1,33 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+#class resumeParser:
+def extractPDFText(filename):
+
+    # following code snippet adapted from
+    # https://stackoverflow.com/questions/26494211/extracting-text-from-a-pdf-file-using-pdfminer-in-python
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    fp = open(filename, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+
+    return text.split()
+
---
+++ resumeParserTest.py
@@ -0,0 +1,10 @@
+import unittest
+from resumeParser import extractPDFText
+
+class resumeParserTest(unittest.TestCase):
+    def testBasic(self):
+        hiText = extractPDFText('Hi.pdf')
+        self.assertEqual(hiText, ['Hi','Hello', 'how', 'are', 'you', 'I’m', 'good', 'thanks', 'Sick'])
+
+if __name__ == '__main__':
+    unittest.main()

55780148e349bef59c43934b80909b04d3c53a29
boyanc <boyan_claire@cat.com>
2020-07-16 12:56:01 -0500

    removed .pyc file, moved example PDFs to folder

--- resumeParserTest.py
+++ resumeParserTest.py
@@ -3,7 +3,7 @@ from resumeParser import extractPDFText
 
 class resumeParserTest(unittest.TestCase):
     def testBasic(self):
-        hiText = extractPDFText('Hi.pdf')
+        hiText = extractPDFText('examplePDFs/Hi.pdf')
         self.assertEqual(hiText, ['Hi','Hello', 'how', 'are', 'you', 'I’m', 'good', 'thanks', 'Sick'])
 
 if __name__ == '__main__':

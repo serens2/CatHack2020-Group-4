9af700af2b139bc12a0bbec877d6e41dce88ee55
boyanc <boyan_claire@cat.com>
2020-07-17 08:35:31 -0500

    implemented threading to speed up phrase-checking

--- resumeParser.py
+++ resumeParser.py
@@ -4,15 +4,29 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from io import StringIO
 import operator
+import threading
 
-ignore_keywords = ['the', 'and', 'a', 'an', 'with', 'just', 'at', 'in', 'I', 'me', 'of', 'on', 'to', 'it', 'like', '.', ',', '\'', '\"', ';', '?', '!', '(', ')', '-', '*']
-finance_keywords = ['finance', 'money', 'portfolio', 'stock', 'stocks', 'economics', 'firm', 'accounting']
-humanResources_keywords = ['human resource', 'human resources', 'social work', 'communication', 'human', 'people', 'people skills', 'interpersonal', 'emotional', 'diversity']
+ignore_keywords = ['the', 'and', 'a', 'an', 'with', 'just', 'at', 'in', 'i', 'me', 'of', 'on', 'to', 'it', 'like', '.', ',', '\'', '\"', ';', '?', '!', '(', ')', '-', '*']
+
+finance_keywords = ['finance', 'money', 'portfolio', 'stock', 'stocks', 'economics', 'firm', 'accounting', 'integrity', 'optimize', 'financed', 'analyze', 'growth',
+                    'middle market', 'strategic', 'private equity', 'capital', 'evaluation']
+
+humanResources_keywords = ['human resource', 'human resources', 'social work', 'communication', 'human', 'people', 'people skills', 'interpersonal', 'emotional',
+                           'diversity', 'budgeting', 'conflict resolution', 'conflict', 'decision making', 'consumer', 'managed', 'soft skills', 'manage', 'teamwork',
+                           'team building', 'team', 'cooperation', 'cooperate', 'logistics', 'organization', 'ethics', 'ethical', 'organize', 'customer', 'relationship',
+                           'management']
+
 softwareDevelopment_keywords = ['cs', 'back end', 'backend', 'front end', 'frontend', 'full stack', 'full stack', 'application', 'features', 'data', 'analytics', 'analysis',
-                       'data analytics', 'data analysis', 'data science', 'science', 'technology', 'security', 'cyber', 'computer', 'computer science', 'support',
-                       'framework', 'terminal', 'java', 'python', 'github', 'developer', 'artificial intelligence', 'artificial', 'ai', 'it', 'machine learning', 'machine',
-                       'virtual', 'cybersecurity', 'security', 'cyber']
-manufacturing_keywords = []
+                                'data analytics', 'data analysis', 'data science', 'science', 'technology', 'security', 'cyber', 'computer', 'computer science', 'support',
+                                'framework', 'terminal', 'java', 'python', 'github', 'developer', 'artificial intelligence', 'artificial', 'ai', 'it', 'machine learning',
+                                'machine', 'virtual', 'cybersecurity', 'security', 'cyber', 'software', 'latex', 'mathematics', 'mips', 'assembly', 'c++', 'programming',
+                                'program', 'code', 'coding', 'javascript', 'sql']
+
+manufacturing_keywords = ['manufacturing', 'manufacture', 'mechanical engineering', 'mechanical', 'mechanics', 'engineering', 'civil engineering', 'civil engineer', 'civil',
+                          'industrial', 'industrial engineering', 'robotic', 'robotics', 'construction', 'build', 'process', 'factory', 'industry', 'construct', 'oversee',
+                          'supply chain', 'transport', 'vehicle', 'equipment', 'automate', 'machinery', 'machine', 'machines', 'robots', 'packaging']
+
+frequencies = {'finance': 0, 'humanResources': 0, 'softwareDevelopment': 0, 'manufacturing': 0}
 
 def extractPDFText(filename):
 
@@ -38,20 +52,82 @@ def extractPDFText(filename):
     retstr.close()
     return text.split()
 
-def parseText(text):
-    frequencies = {'finance' : 0, 'humanResources' : 0, 'softwareDevelopment' : 0, 'manufacturing' : 0}
+def parseFinance(text):
+    val = 0
+    for word in finance_keywords:
+        if ' ' in word:
+            arr = word.split()
+            for i in range(len(text) - 1):
+                if text[i].lower() == arr[0] and text[i + 1].lower() == arr[1]:
+                    val += 1
+        else:
+            if word in text:
+                val +=1
 
-    for word in text:
-        if word.lower() in ignore_keywords:
-            continue
-        if word.lower() in finance_keywords:
-            frequencies['finance'] = frequencies.get('finance') + 1
-        if word.lower() in humanResources_keywords:
-            frequencies['humanResources'] = frequencies.get('humanResources') + 1
-        if word.lower() in softwareDevelopment_keywords:
-            frequencies['softwareDevelopment'] = frequencies.get('softwareDevelopment') + 1
-        if word.lower() in manufacturing_keywords:
-            frequencies['manufacturing'] = frequencies.get('manufacturing') + 1
+    frequencies['finance'] = val
+
+def parseHumanResources(text):
+    val = 0
+    for word in humanResources_keywords:
+        if ' ' in word:
+            arr = word.split()
+            for i in range(len(text) - 1):
+                if text[i].lower() == arr[0] and text[i + 1].lower() == arr[1]:
+                    val += 1
+        else:
+            if word in text:
+                val += 1
+    frequencies['humanResources'] = val
+
+def parseSoftwareDevelopment(text):
+    val = 0
+    for word in softwareDevelopment_keywords:
+        if ' ' in word:
+            arr = word.split()
+            for i in range(len(text) - 1):
+                if text[i].lower() == arr[0] and text[i + 1].lower() == arr[1]:
+                    val += 1
+        else:
+            if word in text:
+                val += 1
+    frequencies['softwareDevelopment'] = val
+
+def parseManufacturing(text):
+    val = 0
+    for word in manufacturing_keywords:
+        if ' ' in word:
+            arr = word.split()
+            for i in range(len(text) - 1):
+                if text[i].lower() == arr[0] and text[i + 1].lower() == arr[1]:
+                    val += 1
+        else:
+            if word in text:
+                val += 1
+    frequencies['manufacturing'] = val
+
+def parseText(text):
+
+    threads = []
+
+    financeTask = threading.Thread(target=parseFinance, args=(text,))
+    threads.append(financeTask)
+    financeTask.start()
+
+    humanResourcesTask = threading.Thread(target=parseHumanResources, args=(text,))
+    threads.append(humanResourcesTask)
+    humanResourcesTask.start()
+
+    softwareDevelopmentTask = threading.Thread(target=parseSoftwareDevelopment, args=(text,))
+    threads.append(softwareDevelopmentTask)
+    softwareDevelopmentTask.start()
+
+    manufacturingTask = threading.Thread(target=parseManufacturing, args=(text,))
+    threads.append(manufacturingTask)
+    manufacturingTask.start()
+
+    for t in threads:
+        t.join()
+
     result = max(frequencies.items(), key=operator.itemgetter(1))[0]
     return result
 
